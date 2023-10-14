ae4a8df1e9706d7516e55baebc4d515671ad7096
rashika-veera <rashikaveera@gmail.com>
2023-10-14 21:42:33 +0530

    episode2

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import * as ReactDOM from 'react-dom';
+
+const heading=React.createElement("div",
+{id:"parent"},
+[React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I am a H1 tag"),
+    React.createElement("h1",{},"I am a H2 tag")
+]),
+React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{},"I am a H1 tag"),
+    React.createElement("h1",{},"I am a H2 tag")
+])]
+    
+    );
+console.log(heading)
+
+const root=ReactDOM.createRoot(document.getElementById("id"))
+root.render(heading);
